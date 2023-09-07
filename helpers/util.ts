a2cb9d1e93d4d6212a3d51a3732964f9cf340857
rembrandtcosta <rembrandt.costa@ccc.ufcg.edu.br>
2023-09-07 14:23:53 -0300

    feat(tool): generate contents randomly

--- helpers/util.ts
+++ helpers/util.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from "crypto";
 import Post from "../models/post";
 import { collections, connectToDatabase } from "../services/database.service";
+import { loremIpsum } from "lorem-ipsum";
 
 export async function setUpDb(n: number) {
   for (let i = 0; i < n; i++) {
@@ -18,15 +19,51 @@ export async function insertPost() {
 function generateRandomPost(): Post {
   const post: Post = {
     "postId": randomUUID(),
-    "title": "test",
-    "content": "test test",
-    "author": "tester",
+    "title": generateRandomTitle(),
+    "content": generateRandomContent(),
+    "author": generateRandomAuthorName(),
     "published": new Date(Date.now()),
-    "coverImage": "https://loremflickr.com/320/240?lock=212",
-    "tags": ["test"],
+    "coverImage": `https://loremflickr.com/1280/720?lock=${generateRandomNumber(2000)}`,
+    "tags": generateRandomTags(generateRandomNumber(7)),
   };
 
   return post;
+}
+
+function generateRandomTitle(): string {
+  return loremIpsum({
+    sentenceUpperBound: 10,
+  }).split(' ').map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }).join(' ');
+}
+
+function generateRandomContent(): string {
+  return loremIpsum({
+    count: generateRandomNumber(6),
+    units: "paragraph"
+  });
+}
+
+function generateRandomAuthorName(): string {
+  return loremIpsum({
+    count: 2,
+    units: "word",
+  }).split(' ').map((name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+  }).join(' ');
+}
+
+function generateRandomNumber(upTo: number): number {
+  return Math.floor(Math.random() * (upTo - 1)) + 1;
+}
+
+function generateRandomTags(n: number): string[] {
+  let words: string[] = []; 
+  while (words.length !== n) {
+    words = [...new Set(loremIpsum({count: n, units:"word"}).split(' '))];
+  }
+  return words;
 }
 
 export function getPage(array: Post[], page: number, perPage: number): Post[] {
